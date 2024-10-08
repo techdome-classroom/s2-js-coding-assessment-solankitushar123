c446f7ce89c2c9aad754af21dd454d65a2b2fac1
Tejasvi Solanki <110277166+solankitushar123@users.noreply.github.com>
2024-10-08 14:19:25 +0530

    program1.js

--- program1.js
+++ program1.js
@@ -2,9 +2,25 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
+function isValid(s) {
+    const stack = [];
+    const bracketMap = { ')': '(', '}': '{', ']': '[' };
     
-};
+    for (let char of s) {
+        if (bracketMap[char]) {
+            const topElement = stack.length > 0 ? stack.pop() : '#';
+            if (topElement !== bracketMap[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    
+    return stack.length === 0;
+}
+
+
 
 module.exports = { isValid };
 
